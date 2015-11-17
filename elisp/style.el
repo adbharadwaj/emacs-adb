994db8ee001a61ef411055e847c69764b8b9d4f8
Aditya Bharadwaj <bharadwaj.adi18@gmail.com>
2015-11-17 15:42:29 -0500

    Initial commit

---
+++ elisp/style.el
@@ -0,0 +1,4 @@
+(require 'linum)
+(line-number-mode 1)
+(column-number-mode 1)  ;; Line numbers on left most column
+(global-linum-mode 1)
---
+++ init.el
@@ -0,0 +1,8 @@
+(require 'package)
+  (push '("marmalade" . "http://marmalade-repo.org/packages/")
+        package-archives )
+  (push '("melpa" . "http://melpa.milkbox.net/packages/")
+        package-archives)
+  (package-initialize)
+
+(fset 'yes-or-no-p 'y-or-n-p)
